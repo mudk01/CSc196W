de1e04aa261aee7aa32037d325cad7fde871b021
MudK01 <khalid.mueed01@gmail.com>
2021-11-16 23:45:48 -0800

    done

--- week5/starter/encrypt-it.js
+++ week5/starter/encrypt-it.js
@@ -1,7 +1,7 @@
 /*
- * Starter file 
+ * Starter file
  */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -17,17 +17,73 @@
   function init() {
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
-    const encryptButton = document.getElementById('encrypt-it')
-    const resetButton = document.getElementById('reset')
-
-    encryptButton.addEventListener("click", encrypt());
-    resetButton.addEventListener("click")
+    encrypt();
   }
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
 
+  function encrypt() {
+    let encryptButton = document.getElementById('encrypt-it')
+    let resetButton = document.getElementById('reset')
+    let textArea = document.getElementById('input-text')
+    let capsCheck = document.getElementById('all-caps')
+    let fontCheck = document.getElementsByName('text-size')
+    let cipherType = document.getElementById('cipher-type')
+    let resultBox = document.getElementById('result')
+
+    encryptButton.addEventListener('click', () => {
+      let userInput = textArea.value
+      let encrypted = ''
+      if (cipherType.value == 'shift') {
+        encrypted = shiftCipher(userInput)
+        console.log('shift')
+      } else if(cipherType.value == 'random') {
+        encrypted = random(userInput)
+        console.log('random')
+      }
+      if (capsCheck.checked) {
+        encrypted = encrypted.toUpperCase()
+      }
+        if (fontCheck[0].checked) {
+          resultBox.style.fontSize = 12
+        }
+        else {
+          resultBox.style.fontSize = 24
+        }
+      resultBox.innerHTML = encrypted
+      
+    })
+    resetButton.addEventListener('click', () => {
+      textArea.value = ''
+      resultBox.innerHTML = ''
+    })
+  }
+
 })();
-function encrypt() {
-  console.log('hello dude')
+
+function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { // letter is between 'a' and 'y'
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+  return result;
+}
+function random(text) {
+  text = text.toLowerCase();
+  let result = "";
+  let num = Math.floor(Math.random() * 15)
+  for (let i = 0; i < text.length; i++) {
+    result += String.fromCharCode(text.charCodeAt(i) + num)
+  }
+  return result;
 }
